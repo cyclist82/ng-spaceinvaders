13c3ee2e9471dcfc7c1a43b9965b0156365a9b9f
Leif Lampater <leiflampater@gmail.com>
2021-03-21 16:44:53 +0100

    chore: test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,12 +20,14 @@ describe(AppComponent.name, () => {
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'ng-spaceinvaders'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+
     expect(app.title).toEqual('ng-spaceinvaders');
   });
 });
